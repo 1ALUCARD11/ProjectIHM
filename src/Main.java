c44da3aa27da2f6f16850b6241326f4096c9b57f
ahmed <ahmedouial765@gmail.com>
2023-12-07 20:36:27 +0100

    adding the database

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,10 @@ public class Main {
 
         Connection connection = Methodes.connect("gestionbibliotheque");
         new MainPanel();
-        for (UIManager.LookAndFeelInfo lafinfo: UIManager.getInstalledLookAndFeels()) {
-            System.out.println(lafinfo.getClassName());
-
-            
-        }
         try {
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (UnsupportedLookAndFeelException e) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 UnsupportedLookAndFeelException e) {
             throw new RuntimeException(e);
         }
         String file_name;
